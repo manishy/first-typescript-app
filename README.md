0151fcc8117aa899e7c09af13ef310d84be50d3c
Manish <manishy@thoughtworks.com>
2020-04-27 14:42:28 +0530

    Add ToDo app with basic features.

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
-## Available Scripts
+## This project was created just to learn TypeScript .
+### Available Scripts
 
 In the project directory, you can run:
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+
+type ToDoProps = {
+  index: number,
+  todo: {
+    text: string
+  }
+};
+
+type ToDoFormProps = {
+  addTodo: (text: string)=>void
+};
+
+const Todo = ({ index, todo }: ToDoProps) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="todo">
+        {todo.text}
+      </div>
   );
-}
+};
+
+const TodoForm = ({addTodo}: ToDoFormProps)=>{
+  const [value, setValue] = useState('');
+  const handleSubmit = (e:any)=>{
+    e.preventDefault();
+    if(!value) return;
+    addTodo(value);
+    setValue('');
+  };
+  return (
+      <form onSubmit ={handleSubmit}>
+        <input type="text" className="input" value={value} onChange={e=>setValue(e.target.value)}/>
+      </form>
+  );
+};
+
+const App = () => {
+  const [toDos, setTodos] = useState([
+    {
+      text: "Have to play cricket",
+      isCompleted: false
+    },
+    {
+      text: "Read book",
+      isCompleted: false
+    },
+    {
+      text: "Cook food",
+      isCompleted: false
+    }]
+  );
+
+  const addTodo= (text: string) =>{
+    const newTodos: any = [... toDos, {text}];
+    setTodos(newTodos);
+  }
+
+  return (
+      <div className="app">
+        <div className="todo-list">
+          {toDos.map((todo, index) => (
+              <Todo key={index} index={index} todo={todo} />))}
+          <TodoForm addTodo={addTodo}/>
+        </div>
+      </div>
+  );
+};
 
 export default App;
