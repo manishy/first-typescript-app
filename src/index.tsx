5bac63bfab499da2a2375f72f2694bda42bb9251
Manish <manishy@thoughtworks.com>
2020-04-30 16:52:23 +0530

    Use Button component just for testing purpose

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// @ts-ignore
+import Button from '@inmanishy/button-component';
 import './index.css';
 import App from './App';
+
 import * as serviceWorker from './serviceWorker';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Button />
   </React.StrictMode>,
   document.getElementById('root')
 );
